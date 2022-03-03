8e8df3e911532d939b1cbbfc7e81a48e9ca54c79
Samip <callmesamip@gmail.com>
2022-03-03 18:24:43 +1100

    Trying to figure out the double fetch request problem

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,15 @@
+import {useState} from 'react'
 const Question = ()=>{
   const wordsURL = 'https://random-word-api.herokuapp.com/word?number=10&swear=0'
   let wordsList=[];
-  const [listOfWords,setListOfWords] = useState(0)
   const fetchWords= async ()=>{
     wordsList = await fetch(wordsURL)
+    .then(res=>res.json())
     .then(data=>wordsList=data);
-
     getData(wordsList)
   }
 const getData=(x)=>{
-  const newWord = x[2];
-  console.log(newWord)
+  console.log(x)
 }
 fetchWords();
   return(
