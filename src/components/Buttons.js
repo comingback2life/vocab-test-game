756291863524105782de5ed11216172a00f034ea
Samip <callmesamip@gmail.com>
2022-03-03 16:29:56 +1100

    Adding a button component

---
+++ src/components/Buttons.js
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Form from 'react-bootstrap/Form'
+export const Buttons =()=>{
+  return(
+    <InputGroup className="mb-3">
+    <Form.Control type="text"
+    />
+  </InputGroup>
+  )
+}
--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,13 +1,17 @@
 import Card from 'react-bootstrap/Card';
 import Title from './Title';
 import{Row,Col} from 'react-bootstrap'
+import { Buttons } from './Buttons';
 const GameCard = ()=>{
   return(
     <Card>
   <Card.Body>
       <Row>
         <Col>
-       <p>My Name is Anthony G</p> 
+        <p> Score : 0.00</p>
+        </Col>
+        <Col className="col-10">
+          <Buttons></Buttons>
         </Col>
         </Row>  
   </Card.Body>
