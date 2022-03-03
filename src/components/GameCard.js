01ca493dda01a94ccca779e74ed47fdd6dc361b7
Samip <callmesamip@gmail.com>
2022-03-03 20:15:35 +1100

    moved the logic from one component to the other

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,9 +1,41 @@
+import {useState} from 'react'
 import Card from 'react-bootstrap/Card';
 import{Row,Col} from 'react-bootstrap'
 import { Buttons } from './Buttons';
 import { AnswerBox } from './AnswerBox';
 import Question from './Question'
+
 const GameCard = ()=>{
+  const [wordDefinition,setWordDefinition]= useState();
+  const wordsURL = 'https://random-word-api.herokuapp.com/word?number=20&swear=0'
+  let wordsList=[];
+
+const getWords =()=>{
+  const fetchWords= async ()=>{
+    wordsList = await fetch(wordsURL)
+    .then(res=>{return res.json()})
+    .then(data=>wordsList.push(data));
+    getWords(wordsList)
+  }
+  fetchWords();
+}
+
+const getData=(x)=>{
+  const randomNumber = Math.ceil(Math.random()*(10-1)+1);
+  console.log(x)
+}
+const getDefintion=()=>{
+  const randomWord= "" || "hello";
+  const definitionURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`
+  const fetchDefinition= async ()=>{
+   await fetch(definitionURL)
+    .then(res=>res.json())
+    .then(data=>{
+    setWordDefinition(data[0].meanings[0].definitions[0].definition)
+    })
+    }
+}
+getData();
   return(
     <Card>
     <Card.Body>
@@ -13,7 +45,7 @@ const GameCard = ()=>{
         <Buttons btnText="Reset"></Buttons>
         </Col>
         <Col className="col-9 text-center">
-          <Question></Question>
+          <Question wordDefinition={wordDefinition}></Question>
           <AnswerBox></AnswerBox>
           <Buttons btnText="Submit"></Buttons>
         </Col>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,20 +1,10 @@
 import {useState} from 'react'
-const Question = ()=>{
-  const wordsURL = 'https://random-word-api.herokuapp.com/word?number=10&swear=0'
-  let wordsList=[];
-  const fetchWords= async ()=>{
-    wordsList = await fetch(wordsURL)
-    .then(res=>res.json())
-    .then(data=>wordsList=data);
-    getData(wordsList)
-  }
-const getData=(x)=>{
-  console.log(x)
-}
-fetchWords();
+const Question = ({wordDefinition})=>{
+  
   return(
     <blockquote className="blockquote mb-3">
     <p>
+      {wordDefinition}
     </p>
   </blockquote>
   )
