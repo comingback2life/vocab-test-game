d9ffff516b7ac6a85f0ca9d2611b1ce5ebb2d6a1
Samip <callmesamip@gmail.com>
2022-03-03 16:48:41 +1100

    Created the footer, looking for ideas to display the question

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import GameCard from './components/GameCard';
 import {Container,Row,Col} from 'react-bootstrap'
+import Footer from './components/Footer'
 import Title from './components/Title';
 function App() {
   return (
+    <div>
     <div className="wrapper">
         <Title/>
       <Container>
         <Row>
        <GameCard></GameCard>
-     
         </Row>
 
       </Container>
-      
-      
     </div>
+    <Footer/>
+    </div>
+    
+    
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+ const Footer = () => {
+  return (
+    <div class="footer text-center">Thank you for playing!</div>
+  )
+}
+export default Footer;
